776935bdd703edebfcb9debd5786848e3247b100
arinachubyr <arina.chubyr@simloud.com>
2023-10-20 02:47:45 +0300

    added desktop html template

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: { base: ["Manrope", "sans-serif"] },
+      colors: {
+        lightBlue: "#00ACFF",
+        darkBlue: "#0A335C",
+        grey: "#1742581A",
+        lightGrey: "#EEF4F6",
+        darkGrey: "#174258CC",
+        lightYellow: "#FFEB99",
+        darkYellow: "#FFCC00",
+      },
+      container: { padding: "32px" },
+      fontSize: { title: "64px" },
+      letterSpacing: { title: "-0.6px" },
+      borderRadius: { lg: "10px" },
+    },
+  },
+};
